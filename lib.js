d57d0b3aa760106fad31e69b0ade5edaa17de445
lj <lj@kingboya.com>
2018-12-04 10:46:04 +0800

    iPhone xr 刘海屏适配

--- lib.js
+++ lib.js
@@ -2,30 +2,41 @@ import {
   View,
   ViewPropTypes as RNViewPropTypes,
   Dimensions,
-  Platform,
-  NativeModules,
-  DeviceInfo
-} from "react-native";
+  Platform
+} from 'react-native';
 
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
 
+// iPhoneX Xs
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
 
-const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get("window");
+// iPhoneXR XsMax
+const XR_WIDTH = 414;
+const XR_HEIGHT = 896;
 
-const { PlatformConstants = {} } = NativeModules;
-const { minor = 0 } = PlatformConstants.reactNativeVersion || {};
+const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get('window');
 
 function isIphoneX() {
-  if (Platform.OS === "web") return false;
-  if (minor >= 50) {
-    return DeviceInfo.isIPhoneX_deprecated;
-  }
+  if (Platform.OS === 'web') return false;
+  return isIphoneXOrXs() || isIphoneXROrXsMAX();
+}
+
+//判断是否为iphoneX或Xs
+function isIphoneXOrXs() {
   return (
-    Platform.OS === "ios" &&
+    Platform.OS === 'ios' &&
     ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) ||
       (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT))
+  );
+}
+
+//判断是否为iphoneXR或XsMAX
+function isIphoneXROrXsMAX() {
+  return (
+    Platform.OS === 'ios' &&
+    ((D_HEIGHT === XR_HEIGHT && D_WIDTH === XR_WIDTH) ||
+      (D_HEIGHT === XR_WIDTH && D_WIDTH === XR_HEIGHT))
   );
 }
 
